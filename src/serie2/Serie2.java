dc18c09e72c0d5fa952b646c1884cd76bfc61c5e
p1210292 <p1210292@IUTAINFOPC59.univ-lyon1.fr>
2014-01-15 10:34:56 +0100

    jhkj

---
+++ src/serie2/Serie2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serie2;
+
+/**
+ *
+ * @author p1210292
+ */
+public class Serie2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
